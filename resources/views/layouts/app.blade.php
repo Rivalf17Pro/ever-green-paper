6743d18459c9794ec81e9b0abd26a67e929d9bdd
Rivalf17Pro <127823700+Rivalf17Pro@users.noreply.github.com>
2023-09-10 17:21:33 -0600

    Se agrega la view de landing y la tabla de contenido de paginas

---
+++ resources/views/layouts/app.blade.php
@@ -0,0 +1,13 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<meta charset="utf-8">
+	<meta http-equiv="X-UA-Compatible" content="IE=edge">
+	<link rel="icon" href="{{ asset('/favicon.ico') }}">
+	<title>@yield('title') | Ever Green Paper</title>
+
+</head>
+<body>
+	@yield('content')
+</body>
+</html>
