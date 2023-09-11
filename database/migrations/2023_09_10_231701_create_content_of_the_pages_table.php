229389db493a75c2b050fbf8c297844bebd85927
Rivalf17Pro <127823700+Rivalf17Pro@users.noreply.github.com>
2023-09-10 18:29:42 -0600

    Se modifican archivos y se incluye TailwindCss

--- database/migrations/2023_09_10_231701_create_content_of_the_pages_table.php
+++ database/migrations/2023_09_10_231701_create_content_of_the_pages_table.php
@@ -14,6 +14,8 @@ return new class extends Migration
         Schema::create('content_of_the_pages', function (Blueprint $table) {
             $table->id();
             $table->timestamps();
+            $table->string('title');
+            $table->text('content');
         });
     }
 
--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -3,11 +3,12 @@
 <head>
 	<meta charset="utf-8">
 	<meta http-equiv="X-UA-Compatible" content="IE=edge">
-	<link rel="icon" href="{{ asset('/favicon.ico') }}">
+	<link rel="icon" href="{{ asset('/favicon.png') }}">
 	<title>@yield('title') | Ever Green Paper</title>
-
+	@vite('resources/css/app.css')
 </head>
 <body>
+	
 	@yield('content')
 </body>
 </html>
